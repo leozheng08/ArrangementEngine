d6069ddc64e0394ae9a715b5b4ca7fcc60a5a1ca
liang.chen <liang.chen@tongdun.cn>
2020-02-09 23:14:59 +0800

    增加平台指标详情

---
+++ biz/src/main/java/cn/tongdun/kunpeng/api/core/rule/detail/IndicatrixDetail.java
@@ -0,0 +1,9 @@
+package cn.tongdun.kunpeng.api.core.rule.detail;
+
+/**
+ * 指标详情的父类
+ * @Author: liang.chen
+ * @Date: 2020/2/6 下午10:27
+ */
+public class IndicatrixDetail extends ConditionDetail{
+}
