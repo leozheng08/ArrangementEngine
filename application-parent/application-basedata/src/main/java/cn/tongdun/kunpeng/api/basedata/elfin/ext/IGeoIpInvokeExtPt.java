7ebc25753d460b97a4a112b51217e9f87d9a6206
liang.chen <liang.chen@tongdun.cn>
2020-02-12 10:26:54 +0800

    增加basedata相关类

---
+++ application-parent/application-basedata/src/main/java/cn/tongdun/kunpeng/api/basedata/elfin/ext/IGeoIpInvokeExtPt.java
@@ -0,0 +1,11 @@
+package cn.tongdun.kunpeng.api.basedata.elfin.ext;
+
+import cn.tongdun.tdframework.core.extension.IExtensionPoint;
+
+/**
+ * 调用手机画像
+ * @Author: liang.chen
+ * @Date: 2020/2/10 下午3:12
+ */
+public interface IMobilePhoneInvokeExtPt extends IExtensionPoint {
+}
