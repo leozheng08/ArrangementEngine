72fc8c65945aaa886f02c98b120244305eb6fe3c
liang.chen <liang.chen@tongdun.cn>
2020-02-17 10:59:44 +0800

    补充异常码

--- engine/src/main/java/cn/tongdun/kunpeng/api/engine/load/bypartner/LoadByPartnerProcessor.java
+++ engine/src/main/java/cn/tongdun/kunpeng/api/engine/load/bypartner/LoadByPartnerProcessor.java
@@ -1,16 +1,11 @@
 package cn.tongdun.kunpeng.api.engine.load.bypartner;
 
-import cn.tongdun.kunpeng.api.engine.load.ILoad;
 import cn.tongdun.kunpeng.api.engine.load.LoadPipeline;
 import cn.tongdun.tdframework.common.dto.Response;
-import cn.tongdun.tdframework.core.exception.SysException;
 import cn.tongdun.tdframework.core.pipeline.PipelineExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 /**
