41b41110c3000f2f42c194c21033b2f1f1e6c533
何奎 <kui.he@tongdun.cn>
2020-03-02 12:28:56 +0800

    自定义缓存加载到缓存

--- engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/rule/function/namelist/CustomListValueManager.java
+++ engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/rule/function/namelist/CustomListValueManager.java
@@ -1,8 +1,11 @@
 package cn.tongdun.kunpeng.api.engine.model.rule.function.namelist;
 
 
+import cn.tongdun.kunpeng.share.kv.IScoreKVRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * 自定义列表数据从数据库加载并设置到缓存
@@ -11,11 +14,35 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class CustomListValueManager {
+    private static final Integer PAGE_SIZE = 10;
 
     @Autowired
     ICustomListValueRepository customListValueRepository;
 
-    public void loadDataToRedis(){
+    @Autowired
+    private IScoreKVRepository redisScoreKVRepository;
 
+    public void loadDataToRedis(){
+        List<String> customLists = customListValueRepository.selectAllAvailable();
+        if (null != customLists) {
+            customLists.stream().forEach(customListUuid -> {
+                int count = customListValueRepository.selectCountByListUuid(customListUuid);
+                if (count >0) {
+                    int totalPage = totalPage(count);
+                    for (int page = 0; page <totalPage; page++) {
+                        List<CustomListValue> list = customListValueRepository.selectByListUuid(customListUuid, page * PAGE_SIZE, PAGE_SIZE);
+                        if (null != list) {
+                            for (CustomListValue listValue : list) {
+                                redisScoreKVRepository.zadd(customListUuid, (double) listValue.getExpireTime(), listValue.getValue());
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    private static int totalPage(int totalCount) {
+        return totalCount % PAGE_SIZE == 0 ? (totalCount / PAGE_SIZE) : (totalCount / PAGE_SIZE) + 1;
     }
 }
