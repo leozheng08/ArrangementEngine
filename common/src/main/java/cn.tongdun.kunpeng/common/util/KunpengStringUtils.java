e63b3e6173ca6ded882fda55562099d6d6b26ad7
liang.chen <liang.chen@tongdun.cn>
2020-03-03 10:18:06 +0800

    修复代码扫描问题

--- common/src/main/java/cn.tongdun.kunpeng/common/util/KunpengStringUtils.java
+++ common/src/main/java/cn.tongdun.kunpeng/common/util/KunpengStringUtils.java
@@ -29,8 +29,12 @@ public class KunpengStringUtils {
     }
 
     public static String lowerCaseFirstChar(String x) {
-        if (x == null) return null;
-        if (x.isEmpty()) return "";
+        if (x == null) {
+            return null;
+        }
+        if (x.isEmpty()) {
+            return "";
+        }
         String firstChar = x.substring(0, 1);
         String exceptFirst = x.substring(1);
         return firstChar.toLowerCase() + exceptFirst;
