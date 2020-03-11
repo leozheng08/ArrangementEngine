9fd7be495d2f2902cec37284cb24f5f45a07e3b4
liang.chen <liang.chen@tongdun.cn>
2020-03-11 18:53:03 +0800

    策略重加载

---
+++ infrastructure/src/main/java/cn/tongdun/kunpeng/api/infrastructure/redis/EventMsgPullRepository.java
@@ -0,0 +1,19 @@
+package cn.tongdun.kunpeng.api.infrastructure.redis;
+
+import cn.tongdun.kunpeng.api.engine.reload.IEventMsgPullRepository;
+import cn.tongdun.kunpeng.common.util.DateUtil;
+
+import java.util.List;
+
+/**
+ * @Author: liang.chen
+ * @Date: 2020/3/11 下午6:42
+ */
+public class EventMsgPullRepository implements IEventMsgPullRepository{
+
+    @Override
+    public List<String> pullLastEventMsgs(){
+        String currentKey = DateUtil.getYYYYMMDDHHMMStr();
+        String lastKey = DateUtil.getLastMinute();
+    }
+}
