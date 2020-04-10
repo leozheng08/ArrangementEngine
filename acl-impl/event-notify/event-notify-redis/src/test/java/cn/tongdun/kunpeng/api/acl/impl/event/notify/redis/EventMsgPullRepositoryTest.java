76afc6791bdf7f52e533900bf2e21049ab4799d0
liang.chen <liang.chen@tongdun.cn>
2020-04-10 19:04:26 +0800

    重命名

---
+++ acl-impl/event-notify/event-notify-redis/src/test/java/cn/tongdun/kunpeng/api/acl/impl/event/notify/redis/EventMsgPullRepositoryTest.java
@@ -0,0 +1,90 @@
+package cn.tongdun.kunpeng.api.acl.impl.event.notify.redis;
+
+
+import cn.tongdun.kunpeng.api.common.util.TestUtil;
+import cn.tongdun.kunpeng.share.kv.IScoreValue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class EventMsgPullRepositoryTest {
+
+    @InjectMocks
+    private RedisDomainEventRepository ruleApplicationService;
+
+    @Test
+    public void deduplicationdTest(){
+
+        Set<IScoreValue> scoreValueSet = getScoreValueSet();
+
+        List<String> result = (List<String>)TestUtil.invokePrivateMethod(ruleApplicationService,"deduplicationd",
+                new Class[]{Set.class},scoreValueSet);
+
+        System.out.println(result);
+
+
+
+    }
+
+
+    private Set<IScoreValue> getScoreValueSet(){
+        Set<IScoreValue> scoreValueSet = new HashSet<>();
+        IScoreValue scoreValue = new IScoreValue() {
+            @Override
+            public Double getScore() {return null; }
+            @Override
+            public String getKey() { return null;}
+
+            @Override
+            public Object getValue() {
+                return "{\"occurredTime\":1579074305123,\"eventType\":\"PolicyCreatedEvent\",\"entity\":\"Policy\",\"data\":[{\"uuid\":\"461e2ebb9ac14d5a8101ab030a5c028a\",\"id\":123,\"name\":\"策略集名称\",\"partnerCode\":\"demo\",\"eventType\":\"loan\",\"eventId\":\"test\",\"appName\":\"ios\"}]}";
+            }
+            @Override
+            public long getTtl() {return 0;}
+        };
+        scoreValueSet.add(scoreValue);
+
+
+        scoreValue = new IScoreValue() {
+            @Override
+            public Double getScore() {return null; }
+            @Override
+            public String getKey() { return null;}
+
+            @Override
+            public Object getValue() {
+                return "{\"occurredTime\":1579074305123,\"eventType\":\"PolicyUpdatedEvent\",\"entity\":\"Policy\",\"data\":[{\"uuid\":\"461e2ebb9ac14d5a8101ab030a5c028a\",\"id\":123,\"name\":\"策略集名称\",\"partnerCode\":\"demo\",\"eventType\":\"loan\",\"eventId\":\"test\",\"appName\":\"ios\"},{\"uuid\":\"461e2ebb9ac14d5a8101ab030a5c1234\",\"id\":456,\"name\":\"策略集名称123\",\"partnerCode\":\"demo\",\"eventType\":\"loan\",\"eventId\":\"test\",\"appName\":\"ios\"}]}";
+            }
+            @Override
+            public long getTtl() {return 0;}
+        };
+        scoreValueSet.add(scoreValue);
+
+
+        scoreValue = new IScoreValue() {
+            @Override
+            public Double getScore() {return null; }
+            @Override
+            public String getKey() { return null;}
+
+            @Override
+            public Object getValue() {
+                return "{\"occurredTime\":1579074300023,\"eventType\":\"PolicyUpdatedEvent\",\"entity\":\"Policy\",\"data\":[{\"uuid\":\"461e2ebb9ac14d5a8101ab030a5c028a\",\"id\":123,\"name\":\"策略集名称3\",\"partnerCode\":\"demo\",\"eventType\":\"loan\",\"eventId\":\"test\",\"appName\":\"ios\"},{\"uuid\":\"461e2ebb9ac14d5a8101ab030a5c1234\",\"id\":456,\"name\":\"策略集名称3\",\"partnerCode\":\"demo\",\"eventType\":\"loan\",\"eventId\":\"test\",\"appName\":\"ios\"}]}";
+            }
+            @Override
+            public long getTtl() {return 0;}
+        };
+        scoreValueSet.add(scoreValue);
+
+
+
+        return scoreValueSet;
+    }
+}
