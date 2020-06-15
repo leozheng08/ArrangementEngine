1919822fb75613444e10c958233151a474e852ec
jasoninsun <1025262651@qq.com>
2020-06-15 17:37:36 +0800

    【Modify】邮箱详情调整

--- api-core/rule-detail/src/main/java/cn/tongdun/kunpeng/api/ruledetail/MailModelDetail.java
+++ api-core/rule-detail/src/main/java/cn/tongdun/kunpeng/api/ruledetail/MailModelDetail.java
@@ -2,6 +2,7 @@ package cn.tongdun.kunpeng.api.ruledetail;
 
 import cn.fraudmetrix.module.tdrule.model.IDetail;
 import cn.fraudmetrix.module.tdrule.util.DetailCallable;
+import lombok.Data;
 
 /**
  *
@@ -10,51 +11,21 @@ import cn.fraudmetrix.module.tdrule.util.DetailCallable;
  * @date 05/27/2020
  *
  */
+@Data
 public class MailModelDetail extends ConditionDetail implements DetailCallable {
 
-    private Double rand;
+    private String ruleDesc;
 
-    private String ranResult;
 
     private String simResult;
 
-    private Long time;
+    private String randResult;
+
+    private String mail;
 
     @Override
     public IDetail call() {
         return this;
-    }
-
-    public Double getRand() {
-        return rand;
-    }
-
-    public void setRand(Double rand) {
-        this.rand = rand;
-    }
-
-    public String getRanResult() {
-        return ranResult;
-    }
-
-    public void setRanResult(String ranResult) {
-        this.ranResult = ranResult;
-    }
-
-    public String getSimResult() {
-        return simResult;
-    }
-
-    public void setSimResult(String simResult) {
-        this.simResult = simResult;
-    }
-
-    public Long getTime() {
-        return time;
-    }
-
-    public void setTime(Long time) {
-        this.time = time;
     }
 
 }
