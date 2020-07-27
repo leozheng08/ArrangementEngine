14ef9661df23a7fcdefc4714ca46f121056bfaed
丁远杭 <yuanhang.ding@tongdun.net>
2020-07-27 16:59:11 +0800

    【Modify】流量比对添加入口

---
+++ api-core/engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/compare/ICompareInfoRepository.java
@@ -0,0 +1,10 @@
+package cn.tongdun.kunpeng.api.engine.model.compare;
+
+/**
+ * @author: yuanhang
+ * @date: 2020-07-27 16:38
+ **/
+public interface ICompareInfoRepository {
+
+    int insertFluid(CompareInfo compareInfo);
+}
---
+++ api-core/infrastructure/src/main/java/cn/tongdun/kunpeng/api/infrastructure/persistence/mybatis/mappers/kunpeng/CompareInfoDAO.java
@@ -0,0 +1,25 @@
+package cn.tongdun.kunpeng.api.infrastructure.persistence.mybatis.mappers.kunpeng;
+
+import cn.tongdun.kunpeng.api.engine.model.compare.CompareInfo;
+
+/**
+ * @author: yuanhang
+ * @date: 2020-07-27 15:52
+ **/
+public interface CompareInfoDAO {
+
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(CompareInfo record);
+
+    int insertSelective(CompareInfo record);
+
+    CompareInfo selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(CompareInfo record);
+
+    int updateByPrimaryKeyWithBLOBs(CompareInfo record);
+
+    int updateByPrimaryKey(CompareInfo record);
+}
