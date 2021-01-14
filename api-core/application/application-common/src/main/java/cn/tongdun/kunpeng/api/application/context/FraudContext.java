0047def747df02b0d2d05d51bac24246e996f51e
丁远杭 <yuanhang.ding@tongdun.net>
2021-01-14 17:58:35 +0800

    【Modify】输出接口等调整

--- api-core/application/application-common/src/main/java/cn/tongdun/kunpeng/api/application/context/FraudContext.java
+++ api-core/application/application-common/src/main/java/cn/tongdun/kunpeng/api/application/context/FraudContext.java
@@ -1,7 +1,13 @@
 package cn.tongdun.kunpeng.api.application.context;
 
 import cn.tongdun.kunpeng.api.common.data.AbstractFraudContext;
+import cn.tongdun.kunpeng.client.data.IOutputField;
+import cn.tongdun.kunpeng.client.data.impl.underline.OutputField;
+import com.google.common.collect.Lists;
 import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: liang.chen
@@ -10,4 +16,21 @@ import lombok.Data;
 @Data
 public class FraudContext extends AbstractFraudContext {
 
+    private List<IOutputField> outputFields = Lists.newArrayList();
+
+    private List<Map<String, Object>> outputIndicatrixes = Lists.newArrayList();
+
+
+    public void appendOutputFields(OutputField outputField) {
+        //如果包含则覆盖,否则就追加
+        int i = outputFields.indexOf(outputField);
+        if (i > -1) {
+            IOutputField exists = outputFields.get(i);
+            exists.setValue(outputField.getValue());
+            exists.setType(outputField.getType());
+            exists.setDesc(outputField.getDesc());
+        } else {
+            outputFields.add(outputField);
+        }
+    }
 }
