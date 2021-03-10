dd92601a1192e46d1a9e57ef986eee3e9eb8042b
anglepei <anglepei1116@163.com>
2021-03-10 19:27:46 +0800

    add: kv-cache-redis-jcache

---
+++ api-extension/acl-impl/kv-cache/kv-cache-redis-jcache/src/main/java/cn/tongdun/kunpeng/api/acl/impl/kvcache/redis/RedisBinaryKVRepository.java
@@ -0,0 +1,78 @@
+package cn.tongdun.kunpeng.api.acl.impl.kvcache.redis;
+
+import cn.fraudmetrix.cache.redis.RedisClient;
+import cn.tongdun.kunpeng.share.kv.Cursor;
+import cn.tongdun.kunpeng.share.kv.IBinaryKVRepository;
+import cn.tongdun.kunpeng.share.kv.IValue;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.annotation.Resource;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+/**
+ * Created by lvyadong on 2020/02/20.
+ */
+public class RedisBinaryKVRepository implements IBinaryKVRepository {
+
+    @Resource(name = "kunPengRedisClient")
+    protected RedisClient kunPengRedisClient;
+
+    @Override
+    public boolean exists(final byte[] key){
+        return kunPengRedisClient.exists(key);
+    }
+
+    @Override
+    public boolean del(final byte[] key) {
+        Long res = kunPengRedisClient.del(key);
+        return res != null && res > 0;
+    }
+
+    @Override
+    public byte[] get(final byte[] key){
+        return kunPengRedisClient.get(key);
+    }
+
+    @Override
+    public boolean set(final byte[] key, final byte[] value){
+        String reply = kunPengRedisClient.set(key, value);
+        return StringUtils.containsIgnoreCase(reply, "OK");
+    }
+
+    @Override
+    public boolean set(final byte[] key, final byte[] value, final long ttlInMilliSeconds){
+        String reply = kunPengRedisClient.set(key, value);
+        if (!StringUtils.containsIgnoreCase(reply, "OK")) {
+            return false;
+        }
+        Long res = kunPengRedisClient.pexpire(key, ttlInMilliSeconds);
+        return res > 0;
+    }
+
+    @Override
+    public long ttl(final byte[] key){
+        try {
+            return kunPengRedisClient.pttl(new String(key,"utf8"));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean setTtl(final byte[] key, final long ttlInMilliSeconds){
+        Long res = kunPengRedisClient.pexpire(key, ttlInMilliSeconds);
+        return res > 0;
+    }
+
+    @Override
+    public List<IValue<byte[],byte[]>> scan(final byte[] prefix, final Cursor cursor, final int count){
+        return null;
+    }
+
+    @Override
+    public void destroy(){
+
+    }
+}
+
