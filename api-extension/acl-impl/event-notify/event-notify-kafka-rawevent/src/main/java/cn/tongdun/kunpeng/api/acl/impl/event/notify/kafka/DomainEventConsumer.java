6eb8fc38e17c057ea91676bd7550b5e617a8849a
changkai.yang <changkai.yang@tongdun.cn>
2021-03-23 17:42:07 +0800

    domain_event_topic支持双机房同步过来的topic，北美aws机房迁移时加

--- api-extension/acl-impl/event-notify/event-notify-kafka-rawevent/src/main/java/cn/tongdun/kunpeng/api/acl/impl/event/notify/kafka/DomainEventConsumer.java
+++ api-extension/acl-impl/event-notify/event-notify-kafka-rawevent/src/main/java/cn/tongdun/kunpeng/api/acl/impl/event/notify/kafka/DomainEventConsumer.java
@@ -2,10 +2,15 @@ package cn.tongdun.kunpeng.api.acl.impl.event.notify.kafka;
 
 import cn.tongdun.kunpeng.api.acl.event.notice.IRawDomainEventHandle;
 import cn.tongdun.kunpeng.share.json.JSON;
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class DomainEventConsumer extends AbstractConsumer {
@@ -15,6 +20,24 @@ public class DomainEventConsumer extends AbstractConsumer {
     @Autowired
     private IRawDomainEventHandle rawDomainEventHandle;
 
+    @Value("${domain.event.topic2.disabled:true}")
+    private String topic2Disabled;
+
+    @Value("${domain.event.topic2:kunpeng_admin_event2,staging_kunpeng_admin_event2}")
+    private String topic2;
+
+    private boolean isTopic2Disabled(){
+        return Boolean.parseBoolean(topic2Disabled);
+    }
+
+    private List<String> topic2List(){
+        if(StringUtils.isNotBlank(topic2)){
+            return Lists.newArrayList(topic2.split(","));
+        }else {
+            return new ArrayList<String>();
+        }
+    }
+
     @Override
     protected boolean batchConsume() {
         return false;
@@ -23,6 +46,10 @@ public class DomainEventConsumer extends AbstractConsumer {
     @Override
     public void onMessage(String topic, Map message) {
         logger.info("DomainEventConsumer consumer message,topic:"+topic+",message:"+ JSON.toJSONString(message));
+        if(isTopic2Disabled() && topic2List().contains(topic)){
+            logger.info("topic2:{} ignore, message:{}", topic, message);
+            return;
+        }
         rawDomainEventHandle.handleRawMessage(message);
     }
 
