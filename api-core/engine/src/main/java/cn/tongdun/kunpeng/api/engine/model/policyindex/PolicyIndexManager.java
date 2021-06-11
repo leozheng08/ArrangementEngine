9b15a67404258b4c6e3d24d43115c733a8e43f9f
anglepei <anglepei1116@163.com>
2021-06-11 16:04:45 +0800

    add: log

--- api-core/engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/policyindex/PolicyIndexManager.java
+++ api-core/engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/policyindex/PolicyIndexManager.java
@@ -1,12 +1,14 @@
 package cn.tongdun.kunpeng.api.engine.model.policyindex;
 
 import cn.fraudmetrix.module.tdrule.function.Function;
-import cn.tongdun.kunpeng.api.engine.IExecutor;
 import cn.tongdun.kunpeng.api.common.data.AbstractFraudContext;
+import cn.tongdun.kunpeng.api.engine.IExecutor;
+import cn.tongdun.kunpeng.share.json.JSON;
 import cn.tongdun.kunpeng.share.utils.TraceUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -23,10 +25,16 @@ public class PolicyIndexManager implements IExecutor<String, Void> {
     @Autowired
     private PolicyIndexCache policyIndexCache;
 
+    @Value("${open.debug.log:false}")
+    private boolean openDebugLog;
+
     @Override
     public Void execute(String policyUuid, AbstractFraudContext context) {
 
         List<PolicyIndex> policyIndexList = policyIndexCache.getList(policyUuid);
+        if (openDebugLog) {
+            logger.info("PolicyIndexManager execute start.....policyUuid={}, policyIndexList={}", policyUuid, JSON.toJSONString(policyIndexList));
+        }
         if (null == policyIndexList || policyIndexList.isEmpty()) {
             return null;
         }
@@ -36,6 +44,9 @@ public class PolicyIndexManager implements IExecutor<String, Void> {
                 continue;
             }
             Object object = calculateFunction.eval(context);
+            if (openDebugLog) {
+                logger.info("PolicyIndexManager execute .....policyUuid={}, policyIndex={}, result={}", policyUuid, JSON.toJSONString(policyIndex), object);
+            }
             if (null != object) {
                 context.putPolicyIndex(policyIndex.getUuid(), convert2Double(object, policyIndex));
             }
@@ -55,7 +66,7 @@ public class PolicyIndexManager implements IExecutor<String, Void> {
         try {
             return Double.parseDouble(valueStr);
         } catch (Exception e) {
-            logger.error(TraceUtils.getFormatTrace()+"PolicyIndexManager convert2Double error,valueString:" + valueStr + ",policyIndexUuid:" + policyIndex.getUuid());
+            logger.error(TraceUtils.getFormatTrace() + "PolicyIndexManager convert2Double error,valueString:" + valueStr + ",policyIndexUuid:" + policyIndex.getUuid());
             throw e;
         }
     }
