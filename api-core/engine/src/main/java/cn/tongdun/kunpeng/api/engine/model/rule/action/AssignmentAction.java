78aeee8668a507a9846ae90bc6bea92e3c2fdaa1
mengtao <tao.meng002892>
2021-10-21 19:44:41 +0800

    权重模式下，rule中操作配置后规则reload不成功。

--- api-core/engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/rule/action/AssignmentAction.java
+++ api-core/engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/rule/action/AssignmentAction.java
@@ -43,6 +43,9 @@ public class AssignmentAction implements Action {
             case "indicatrix":
                 right = new PlatformIndex(rightValue, false, null);
                 break;
+            case "index":
+                right = new PolicyIndex(rightValue);
+                break;
             default:
                 throw new ParseException("AssignmentAction parse error!");
         }
