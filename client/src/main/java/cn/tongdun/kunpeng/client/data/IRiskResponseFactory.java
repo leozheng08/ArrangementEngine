262e949ba3b2e77e77bd9a5a8784212db290cf88
汪忠祥 <1186355422@qq.com>
2021-10-21 18:57:57 +0800

    add；添加序列化

--- client/src/main/java/cn/tongdun/kunpeng/client/data/IRiskResponseFactory.java
+++ client/src/main/java/cn/tongdun/kunpeng/client/data/IRiskResponseFactory.java
@@ -1,10 +1,12 @@
 package cn.tongdun.kunpeng.client.data;
 
+import java.io.Serializable;
+
 /**
  * @Author: liang.chen
  * @Date: 2020/3/2 上午10:05
  */
-public interface IRiskResponseFactory {
+public interface IRiskResponseFactory extends Serializable {
     IRiskResponse newRiskResponse();
     IHitRule newHitRule();
     IOutputField newOutputField();
--- client/src/main/java/cn/tongdun/kunpeng/client/dto/WeightedRiskConfigDTO.java
+++ client/src/main/java/cn/tongdun/kunpeng/client/dto/WeightedRiskConfigDTO.java
@@ -2,13 +2,15 @@ package cn.tongdun.kunpeng.client.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 权重匹配风险配置，内容为：{"mode":"Weighted","riskWeight":10,"weightRatio":20.33,"op":"+","property":{"type":"indicatrix/field","name":"指标/字段"},"propertyValue":{"value":"3333333333"},"upperLimitScore":-30,"lowerLimitScore":30}
  * @Author: liang.chen
  * @Date: 2020/2/23 下午12:13
  */
 @Data
-public class WeightedRiskConfigDTO{
+public class WeightedRiskConfigDTO implements Serializable {
 
     /**
      * 权重模式有下面的值
