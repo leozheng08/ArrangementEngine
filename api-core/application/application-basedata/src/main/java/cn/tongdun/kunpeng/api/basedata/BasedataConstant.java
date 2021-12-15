c37ed36953dc642ed2615138a0ab4a963b3a2b03
angle <angle@angledeMacBook-Air.local>
2021-12-15 13:27:04 +0800

    add: MobilePhoneStep默认实现

--- api-core/application/application-basedata/src/main/java/cn/tongdun/kunpeng/api/basedata/BasedataConstant.java
+++ api-core/application/application-basedata/src/main/java/cn/tongdun/kunpeng/api/basedata/BasedataConstant.java
@@ -6,8 +6,18 @@ package cn.tongdun.kunpeng.api.basedata;
  */
 public class BasedataConstant {
 
+    /**
+     * 通过geoIp获得的访问ip对应的相关地理位置信息
+     */
     public static final String EXTERNAL_OBJ_GEOIP_ENTITY = "geoipEntity";
+    /**
+     * IP画像
+     */
     public static final String EXTERNAL_OBJ_IP_REPUTATION = "ipReputationRulesObj";
+    /**
+     * 手机画像规则对象
+     */
+    public static final String EXTERNAL_OBJ_MOBILE_PHONE = "mobilePhoneRuleObj";
     public static final String IP_ADDRESS = "ipAddress";
     public static final String TRUE_IP_ADDRESS = "trueIpAddress";
     public static final String MOBILE_ADDRESS = "mobileAddress";
--- api-core/application/application-basedata/src/main/java/cn/tongdun/kunpeng/api/basedata/step/mobile/ext/MobilePhoneInvokeExt.java
+++ api-core/application/application-basedata/src/main/java/cn/tongdun/kunpeng/api/basedata/step/mobile/ext/MobilePhoneInvokeExt.java
@@ -1,14 +1,20 @@
 package cn.tongdun.kunpeng.api.basedata.step.mobile.ext;
 
+import cn.tongdun.kunpeng.api.common.data.AbstractFraudContext;
 import cn.tongdun.kunpeng.api.common.data.BizScenario;
+import cn.tongdun.kunpeng.client.data.IRiskResponse;
+import cn.tongdun.kunpeng.client.data.RiskRequest;
 import cn.tongdun.tdframework.core.extension.Extension;
 
 /**
  * @Author: liang.chen
  * @Date: 2020/2/10 下午9:44
  */
-@Extension(business = BizScenario.DEFAULT,tenant = BizScenario.DEFAULT,partner = BizScenario.DEFAULT)
+@Extension(business = BizScenario.DEFAULT, tenant = BizScenario.DEFAULT, partner = BizScenario.DEFAULT)
 public class MobilePhoneInvokeExt implements IMobilePhoneInvokeExtPt {
 
-
+    @Override
+    public boolean fetchData(AbstractFraudContext context, IRiskResponse response, RiskRequest request) {
+        return true;
+    }
 }
