ff23034c3711f4dcd0ffad7991cf6c2ba2b928d9
mengtao <tao.meng002892>
2021-12-27 16:06:07 +0800

    冠军挑战者适配：修改

--- api-core/application/application-activity/application-activity-common/src/main/java/cn/tongdun/kunpeng/api/application/activity/common/ActivityStoreKafkaWorker.java
+++ api-core/application/application-activity/application-activity-common/src/main/java/cn/tongdun/kunpeng/api/application/activity/common/ActivityStoreKafkaWorker.java
@@ -78,7 +78,7 @@ public class ActivityStoreKafkaWorker implements IEventWorker {
         IActitivyMsg actitivyMsg = generateActivity(item);
 
         //发送到kafka originalSeqId发送不同的topic
-        Object originalSeqId = item.getContext().getFieldValues().get("originalSeqId") == null ? item.getContext().getRiskRequest().getFieldValues().get("originalSeqId") : null;
+        Object originalSeqId = item.getContext().getFieldValues().get("originalSeqId") != null ? item.getContext().getFieldValues().get("originalSeqId") : item.getContext().getRiskRequest().getFieldValues().get("originalSeqId");
         if (Objects.nonNull(originalSeqId)) {
             sendChallengerToKafka(actitivyMsg);
         } else {
