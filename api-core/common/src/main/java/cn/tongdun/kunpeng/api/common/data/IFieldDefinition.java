40d42217880d59ba9c9b16f8c5c1a42e97b380bc
angle <angle@angledeMacBook-Air.local>
2022-01-17 21:27:24 +0800

    update: 字段管理增应用维度，应用缓存增加合作方应用map

--- api-core/common/src/main/java/cn/tongdun/kunpeng/api/common/data/IFieldDefinition.java
+++ api-core/common/src/main/java/cn/tongdun/kunpeng/api/common/data/IFieldDefinition.java
@@ -21,4 +21,7 @@ public interface IFieldDefinition {
 
     boolean isExtField();
 
+    String getAppName();
+
+
 }
--- api-core/engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/application/AdminApplicationCache.java
+++ api-core/engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/application/AdminApplicationCache.java
@@ -4,9 +4,13 @@ import cn.tongdun.kunpeng.api.acl.engine.model.application.AdminApplicationDTO;
 import cn.tongdun.kunpeng.api.acl.engine.model.application.IAdminApplicationRepository;
 import cn.tongdun.kunpeng.api.engine.cache.AbstractLocalCache;
 import cn.tongdun.kunpeng.share.utils.TraceUtils;
-import com.google.common.cache.*;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.cache.RemovalListener;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+import com.google.common.collect.Sets;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,44 +19,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
  * 应用信息缓存 appName -> AdminApplication
+ *
  * @Author: liang.chen
  * @Date: 2019/12/16 下午8:01
  */
 @Component
-public class AdminApplicationCache extends AbstractLocalCache<String,AdminApplication> {
+public class AdminApplicationCache extends AbstractLocalCache<String, AdminApplication> {
 
     private static final Logger logger = LoggerFactory.getLogger(AdminApplicationCache.class);
 
     //patnerCode.appName -> AdminApplication
     private LoadingCache<String, AdminApplication> adminApplicationTimingCache;
 
+    //patnerCode -> appName
+    private BiMap<String, Set<String>> partnerApplicationCache = HashBiMap.create();
+
     //patnerCode.appName -> secretkey
-    private BiMap<String,String> appNameToSecretKey = HashBiMap.create();
+    private BiMap<String, String> appNameToSecretKey = HashBiMap.create();
     //secretkey -> patnerCode.appName
-    private BiMap<String,String> secretKeyToAppName =  appNameToSecretKey.inverse();
+    private BiMap<String, String> secretKeyToAppName = appNameToSecretKey.inverse();
 
     @Autowired
     private IAdminApplicationRepository iAdminApplicationRepository;
 
     @PostConstruct
-    public void init(){
+    public void init() {
         register(AdminApplication.class);
 
-        final CacheLoader<String,AdminApplication> loader = new CacheLoader<String, AdminApplication>() {
+        final CacheLoader<String, AdminApplication> loader = new CacheLoader<String, AdminApplication>() {
             @Override
             public AdminApplication load(String key) throws Exception {
                 return loadByKey(key);
             }
         };
 
-        RemovalListener<String,AdminApplication> removalListener = notification -> {
+        RemovalListener<String, AdminApplication> removalListener = notification -> {
             AdminApplication value = notification.getValue();
-            if(null == value){
+            if (null == value) {
                 appNameToSecretKey.remove(value.getAppName());
                 secretKeyToAppName.remove(value.getSecretKey());
             }
@@ -62,25 +71,25 @@ public class AdminApplicationCache extends AbstractLocalCache<String,AdminApplic
     }
 
     private AdminApplication loadByKey(String key) {
-        AdminApplication result =  new AdminApplication();
+        AdminApplication result = new AdminApplication();
         try {
             String[] values = StringUtils.split(key, ".");
-            if(values != null && values.length == 2){
+            if (values != null && values.length == 2) {
                 AdminApplicationDTO adminApplicationDTO = iAdminApplicationRepository.selectApplicationByPartnerAppName(values[0], values[1]);
                 AdminApplication adminApplication = new AdminApplication();
-                BeanUtils.copyProperties(adminApplicationDTO,adminApplication);
+                BeanUtils.copyProperties(adminApplicationDTO, adminApplication);
                 result = adminApplication;
             } else {
                 logger.error("load partnerCode and appName key invalidate :{}", key);
             }
         } catch (Exception e) {
-            logger.error("load partnerCode and appName"+ key +" :{}"  , e);
+            logger.error("load partnerCode and appName" + key + " :{}", e);
         }
         return result;
     }
 
     @Override
-    public AdminApplication get(String key){
+    public AdminApplication get(String key) {
         try {
             AdminApplication adminApplication = adminApplicationTimingCache.get(key);
             return adminApplication == null || StringUtils.isEmpty(adminApplication.getSecretKey()) ? null : adminApplication;
@@ -90,9 +99,9 @@ public class AdminApplicationCache extends AbstractLocalCache<String,AdminApplic
         }
     }
 
-    public AdminApplication get(String partnerCode, String appName){
+    public AdminApplication get(String partnerCode, String appName) {
         try {
-            AdminApplication adminApplication = adminApplicationTimingCache.get(generateKey(partnerCode,appName));
+            AdminApplication adminApplication = adminApplicationTimingCache.get(generateKey(partnerCode, appName));
             return adminApplication == null || StringUtils.isEmpty(adminApplication.getSecretKey()) ? null : adminApplication;
         } catch (ExecutionException e) {
             logger.error(TraceUtils.getFormatTrace() + "adminApplicationTimingCache.get error", e);
@@ -101,9 +110,15 @@ public class AdminApplicationCache extends AbstractLocalCache<String,AdminApplic
     }
 
     @Override
-    public void put(String key, AdminApplication adminApplication){
-        adminApplicationTimingCache.put(key,adminApplication);
-        appNameToSecretKey.forcePut(key,adminApplication.getSecretKey());
+    public void put(String key, AdminApplication adminApplication) {
+        adminApplicationTimingCache.put(key, adminApplication);
+        appNameToSecretKey.forcePut(key, adminApplication.getSecretKey());
+        Set<String> appSet = partnerApplicationCache.get(adminApplication.getPartnerCode());
+        if (appSet == null) {
+            appSet = Sets.newHashSet();
+            partnerApplicationCache.put(adminApplication.getPartnerCode(), appSet);
+        }
+        appSet.add(adminApplication.getAppName());
     }
 
     @Override
@@ -120,27 +135,25 @@ public class AdminApplicationCache extends AbstractLocalCache<String,AdminApplic
     }
 
 
-    public void put(String partnerCode, String appName, AdminApplication adminApplication){
-        put(generateKey(partnerCode,appName),adminApplication);
+    public void put(String partnerCode, String appName, AdminApplication adminApplication) {
+        put(generateKey(partnerCode, appName), adminApplication);
     }
 
-    public void remove(AdminApplication adminApplication){
-        String key = generateKey(adminApplication.getPartnerCode(),adminApplication.getAppName());
+    public void remove(AdminApplication adminApplication) {
+        String key = generateKey(adminApplication.getPartnerCode(), adminApplication.getAppName());
         remove(key);
     }
 
 
-
-
     public String generateKey(String partnerCode, String appName) {
-        return StringUtils.join(partnerCode,".",appName);
+        return StringUtils.join(partnerCode, ".", appName);
 
     }
 
 
-    public AdminApplication getBySecretKey(String secretKey){
+    public AdminApplication getBySecretKey(String secretKey) {
         String partnerCodeAppName = secretKeyToAppName.get(secretKey);
-        if(partnerCodeAppName == null){
+        if (partnerCodeAppName == null) {
             return null;
         }
         return get(partnerCodeAppName);
@@ -152,8 +165,12 @@ public class AdminApplicationCache extends AbstractLocalCache<String,AdminApplic
     }
 
 
-    public void addAdminApplication(AdminApplication adminApplication){
-        String key = generateKey(adminApplication.getPartnerCode(),adminApplication.getAppName());
-        put(key,adminApplication);
+    public void addAdminApplication(AdminApplication adminApplication) {
+        String key = generateKey(adminApplication.getPartnerCode(), adminApplication.getAppName());
+        put(key, adminApplication);
+    }
+
+    public Set<String> getApplications(String partnerCode) {
+        return partnerApplicationCache.get(partnerCode);
     }
 }
