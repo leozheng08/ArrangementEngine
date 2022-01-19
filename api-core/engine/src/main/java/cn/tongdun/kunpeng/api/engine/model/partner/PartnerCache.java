53c21b34a226acdd853ae3381cee445ab4e2e887
angle <angle@angledeMacBook-Air.local>
2022-01-19 14:26:42 +0800

    PartnerCache add getAllPartnerCodes

--- api-core/engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/partner/PartnerCache.java
+++ api-core/engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/partner/PartnerCache.java
@@ -1,42 +1,48 @@
 package cn.tongdun.kunpeng.api.engine.model.partner;
 
 import cn.tongdun.kunpeng.api.engine.cache.AbstractLocalCache;
-import cn.tongdun.kunpeng.api.engine.model.subpolicy.SubPolicy;
+import com.google.common.collect.Lists;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 合作方信息缓存partnerCode -> Partner
+ *
  * @Author: liang.chen
  * @Date: 2019/12/16 下午8:01
  */
 @Component
-public class PartnerCache extends AbstractLocalCache<String,Partner> {
+public class PartnerCache extends AbstractLocalCache<String, Partner> {
 
     //partnerCode -> Partner
-    private Map<String,Partner> partnerMap = new ConcurrentHashMap<>();
+    private Map<String, Partner> partnerMap = new ConcurrentHashMap<>();
 
 
     @PostConstruct
-    public void init(){
+    public void init() {
         register(Partner.class);
     }
 
     @Override
-    public Partner get(String partnerCode){
+    public Partner get(String partnerCode) {
         return partnerMap.get(partnerCode);
     }
 
     @Override
-    public void put(String partnerCode, Partner partner){
-        partnerMap.put(partnerCode,partner);
+    public void put(String partnerCode, Partner partner) {
+        partnerMap.put(partnerCode, partner);
     }
 
     @Override
-    public Partner remove(String partnerCode){
+    public Partner remove(String partnerCode) {
         return partnerMap.remove(partnerCode);
+    }
+
+    public List<String> getAllPartnerCodes() {
+        return Lists.newArrayList(partnerMap.keySet());
     }
 }
