965a03ec520ec03f62f0cfe1a08a650203d77b64
angle <angle@angledeMacBook-Air.local>
2022-01-24 21:02:22 +0800

    add: log

--- api-core/engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/rule/RuleManager.java
+++ api-core/engine/src/main/java/cn/tongdun/kunpeng/api/engine/model/rule/RuleManager.java
@@ -1,11 +1,14 @@
 package cn.tongdun.kunpeng.api.engine.model.rule;
 
 import cn.fraudmetrix.module.tdrule.rule.RuleResult;
-import cn.tongdun.kunpeng.api.engine.IExecutor;
 import cn.tongdun.kunpeng.api.common.data.AbstractFraudContext;
-import cn.tongdun.kunpeng.api.common.data.RuleResponse;
 import cn.tongdun.kunpeng.api.common.data.ReasonCode;
+import cn.tongdun.kunpeng.api.common.data.RuleResponse;
 import cn.tongdun.kunpeng.api.common.data.SubReasonCode;
+import cn.tongdun.kunpeng.api.engine.IExecutor;
+import cn.tongdun.kunpeng.share.utils.TraceUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,58 +23,66 @@ public class RuleManager implements IExecutor<String, RuleResponse> {
 
     public final static Number NaN = Double.NaN;
 
+    private static Logger logger = LoggerFactory.getLogger(RuleManager.class);
+
     @Autowired
     RuleCache ruleCache;
 
     @Override
     public RuleResponse execute(String uuid, AbstractFraudContext context) {
-        RuleResponse ruleResponse = new RuleResponse();
+        try {
+            RuleResponse ruleResponse = new RuleResponse();
 
-        Rule rule = ruleCache.get(uuid);
-        if (rule == null || rule.getEval() == null) {
-            context.addSubReasonCode(new SubReasonCode(ReasonCode.RULE_LOAD_ERROR.getCode(), ReasonCode.RULE_LOAD_ERROR.getDescription(), "决策引擎执行"));
-            ruleResponse.setSuccess(false);
-            return ruleResponse;
-        }
-
-        RuleResult ruleResult = rule.getEval().eval(context);
-        if (null == ruleResult || ruleResult.getException() != null || ruleResult.getEvalResult() == null) {
-            context.addSubReasonCode(new SubReasonCode(ReasonCode.RULE_ENGINE_ERROR.getCode(), ReasonCode.RULE_ENGINE_ERROR.getDescription(), "决策引擎执行"));
-            context.removeFunctionDetail(uuid);
-            ruleResponse.setSuccess(false);
-        } else {
-            switch (ruleResult.getEvalResult()) {
-                case True:
-                    ruleResponse.setHit(true);
-                    ruleResponse.setDecision(rule.getDecision());
-                    ruleResponse.setScore(getWeight(rule, context));
-                    break;
-                case False:
-                    ruleResponse.setHit(false);
-                    context.removeFunctionDetail(uuid);
-                    break;
-                case Terminate:
-                    //subPolicy在执行时，如果某条规则返回Terminate=true，则不再执行后继规则。
-                    ruleResponse.setTerminate(true);
-                    context.removeFunctionDetail(uuid);
-                    ruleResponse.setHit(true);
-                    ruleResponse.setDecision(rule.getDecision());
-                    ruleResponse.setScore(getWeight(rule, context));
-                    break;
-                default:
-                    context.addSubReasonCode(new SubReasonCode(ReasonCode.RULE_ENGINE_ERROR.getCode(), ReasonCode.RULE_ENGINE_ERROR.getDescription(), "决策引擎执行"));
-                    context.removeFunctionDetail(uuid);
+            Rule rule = ruleCache.get(uuid);
+            if (rule == null || rule.getEval() == null) {
+                context.addSubReasonCode(new SubReasonCode(ReasonCode.RULE_LOAD_ERROR.getCode(), ReasonCode.RULE_LOAD_ERROR.getDescription(), "决策引擎执行"));
+                ruleResponse.setSuccess(false);
+                return ruleResponse;
             }
-            ruleResponse.setCostTime(ruleResult.getCost());
-            ruleResponse.setSuccess(true);
+
+            RuleResult ruleResult = rule.getEval().eval(context);
+            if (null == ruleResult || ruleResult.getException() != null || ruleResult.getEvalResult() == null) {
+                context.addSubReasonCode(new SubReasonCode(ReasonCode.RULE_ENGINE_ERROR.getCode(), ReasonCode.RULE_ENGINE_ERROR.getDescription(), "决策引擎执行"));
+                context.removeFunctionDetail(uuid);
+                ruleResponse.setSuccess(false);
+            } else {
+                switch (ruleResult.getEvalResult()) {
+                    case True:
+                        ruleResponse.setHit(true);
+                        ruleResponse.setDecision(rule.getDecision());
+                        ruleResponse.setScore(getWeight(rule, context));
+                        break;
+                    case False:
+                        ruleResponse.setHit(false);
+                        context.removeFunctionDetail(uuid);
+                        break;
+                    case Terminate:
+                        //subPolicy在执行时，如果某条规则返回Terminate=true，则不再执行后继规则。
+                        ruleResponse.setTerminate(true);
+                        context.removeFunctionDetail(uuid);
+                        ruleResponse.setHit(true);
+                        ruleResponse.setDecision(rule.getDecision());
+                        ruleResponse.setScore(getWeight(rule, context));
+                        break;
+                    default:
+                        context.addSubReasonCode(new SubReasonCode(ReasonCode.RULE_ENGINE_ERROR.getCode(), ReasonCode.RULE_ENGINE_ERROR.getDescription(), "决策引擎执行"));
+                        context.removeFunctionDetail(uuid);
+                }
+                ruleResponse.setCostTime(ruleResult.getCost());
+                ruleResponse.setSuccess(true);
+            }
+
+            ruleResponse.setId(rule.getRuleCustomId() != null ? rule.getRuleCustomId() : rule.getRuleId());
+            ruleResponse.setName(rule.getName());
+            ruleResponse.setUuid(rule.getUuid());
+            ruleResponse.setParentUuid(rule.getParentUuid());
+
+            return ruleResponse;
+        } catch (Exception e) {
+            logger.error(TraceUtils.getFormatTrace() + "RuleManager::execute error,uuid:" + uuid, e);
+            throw e;
         }
 
-        ruleResponse.setId(rule.getRuleCustomId() != null ? rule.getRuleCustomId() : rule.getRuleId());
-        ruleResponse.setName(rule.getName());
-        ruleResponse.setUuid(rule.getUuid());
-        ruleResponse.setParentUuid(rule.getParentUuid());
-
-        return ruleResponse;
     }
 
     private Integer getWeight(Rule rule, AbstractFraudContext context) {
