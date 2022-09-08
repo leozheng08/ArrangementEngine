83ff0a7bf9ff592de1b1649aaec605c4b3ee8895
tingchang.fan <tingchang.fan@tongdun.net>
2022-09-08 15:30:34 +0800

    【update】: 增加接口说明

--- api-core/application/application-basedata/src/main/java/cn/tongdun/kunpeng/api/basedata/service/LucDynamicConfig.java
+++ api-core/application/application-basedata/src/main/java/cn/tongdun/kunpeng/api/basedata/service/LucDynamicConfig.java
@@ -1,6 +1,8 @@
 package cn.tongdun.kunpeng.api.basedata.service;
 
 /**
+ * 用于LUC动态配置的接口，已默认实现，若有不同，需在业务层实现该接口
+ *
  * @author : tingchang.fan
  * @projectName：kunpeng-api
  * @date 2022/9/8 2:22 下午
